5089c29b2016d5a0b2faf7cac0e25b2916382c15
Caleb Norfleet <caleb_norfleet@caryacademy.org>
2017-11-09 14:46:38 -0800

    Finished v1 of code for claiming far beacon #1
    Not yet tested

--- robot_code/robot_code.ino
+++ robot_code/robot_code.ino
@@ -14,58 +14,60 @@ boolean whiteTeam;
 #define circleThreshold 940
 
 //line following speeds
-#define outSpeed 0.8
+#define outSpeed1 0.8
 #define outSpeed2 -0.2
-#define inSpeed 1
+#define inSpeed1 1
 #define inSpeed2 0.5
 
 void setup()
 {
   Serial.begin(9600);
+  initMotors();
+  initSensors();
+  initServo();
   pinMode(LEDpin, OUTPUT);
   pinMode(teamPin, INPUT);
   digitalWrite(LEDpin, LOW);
   delay(200);
   whiteTeam = (digitalRead(teamPin) == LOW);
-
-  //get to far beacon #3://
-  while(readIrReflect() < lineThreshold)
-  { forward(); delay(5); }
-
-  //turn a bit
-  setR(125);
-  setL(80);
-  delay(500);
-  halt();
-
-  //follow line
-  while(readBeacon != -3)
-  {
-    int r = readIrReflect();
-    if(r < lineThreshold) //turn left
-    { 
-      //setR
-    }
-    else //turn right
-    {
-      
-    }
-  }
 }
 
 void loop()
 {
-  /*
-  Serial.println(readCode());
-  flashCode(5);
-  delay(10);
-  flashCode(6);
-  delay(10);
-  flashCode(7);
-  delay(10);
-  flashCode(8);
-  delay(10);
-  flashCode(9);
-  delay(10);
-  */
+  //get to far beacon #1 (as white team)://
+  //behaviors:
+  //if see claimed beacon, stop moving
+  //if see unclaimed beacon, keep moving but go slower
+  //if on line turn right
+  //if lost, wide turn left
+  boolean done = false;
+  while(!done)
+  {
+    //setup
+    int moveSpeed = 255;
+    int beaconAt = readBeaconWell();
+    Serial.print(beaconAt);
+    int ir = readIrReflect();
+
+    //react based on beacon at
+    if(beaconAt == 1)
+    { done = true; continue; }
+    else if(beaconAt == -1)
+    { moveSpeed = 200; }
+    
+    //moved based on irSensor
+    Serial.print(", "); Serial.println(ir);
+    if(ir > lineThreshold)
+    {
+      setL((int) (inSpeed1 * moveSpeed));
+      setR((int) (inSpeed2 * moveSpeed));
+    }
+    else
+    {
+      setR((int) (outSpeed1 * moveSpeed));
+      setL((int) (outSpeed2 * moveSpeed));
+    }
+  }
+  while(true) //after done, just stop
+  { halt(); delay(50); }
 }
